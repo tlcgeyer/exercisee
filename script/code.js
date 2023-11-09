04c18c44fec5f0ec7c00e8028d129bcc19f2cefa
tlcgeyer <tamlincupido2402@gmail.com>
2023-11-09 09:58:15 +0200

    exercisee

--- script/code.js
+++ script/code.js
@@ -1,5 +1,14 @@
-let numb1 = "1" 
-let numb2 = "2"
-console.log(
-    eval(`${numb1} + ${numb2}`)
-)
+//addition button
+const btnAdd = document.querySelector('[data-submit]')
+const lblOutput = document.querySelector('#output')
+
+function addition () {
+    let numb1 = document.getElementById('numb1').value
+    let numb2 = document.getElementById('numb2').value
+    /*to get the value to the input element*/  
+    //to wrap everything together add .value
+    let sum =eval(`${numb1}+ ${numb2}`)
+    //lblOutput.innerHTML = `<strong>$ {sum}</strong>`
+    lblOutput.textContent = sum
+}
+btnAdd.addEventListener('click', addition)
